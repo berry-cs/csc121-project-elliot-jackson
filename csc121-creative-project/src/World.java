59f7ccf8f1ae9fc7c62d6d92ce7f3ae545ac8b49
jPelot <jackson.pelot@vikings.berry.edu>
2024-09-30 10:27:24 -0400

    Refactor to new changes
    Removed old standalone Note code
    Changed class constructor params
    Changed to use pf

--- csc121-creative-project/src/World.java
+++ csc121-creative-project/src/World.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Objects;
 
 import processing.core.PApplet;
@@ -7,23 +6,18 @@ import processing.event.MouseEvent;
 
 
 public class World implements IWorld{
-	int x;
-	int y;
 	
-	ArrayList<Note> notes;
+	PlayField pf;
 	
-	public World(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public World() {
 		
-		notes = new ArrayList<Note>();
-		notes.add(new Note(50,200));
-		
+		pf = new PlayField(5,5);
 	}
 	
 	public int hashCode() {
-		return Objects.hash(x, y);
+		return Objects.hash(pf);
 	}
+	
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -32,40 +26,22 @@ public class World implements IWorld{
 		if (getClass() != obj.getClass())
 			return false;
 		World other = (World) obj;
-		return x == other.x && y == other.y;
+		return Objects.equals(pf, other.pf);
 	}
 	
-	public PApplet draw(PApplet w) {
-		w.background(255); // white background
-		w.fill(0, 0, 255); // solid blue	
-		for(Note n : notes) {
-			n.draw(w);
-		}
-		
-		return w;
+	public PApplet draw(PApplet p) {
+		p.background(200);
+		pf.draw(p);
+		return p;
 	}
 	
 	public IWorld update(){
-		
-		for(Note n : notes) {
-			n.update();
-		}
-		
-		/*
-		if (this.y < 400) {
-			return new World(this.x, this.y + 1);
-		}
-		else {
-			return this;
-		}
-		*/
+		pf.update();
 		return this;
 	}
 	
 	public IWorld mousePressed(MouseEvent mev) {
-		System.out.println("press");
-		notes.remove(0);
-		notes.add(new Note(50,200));
+		pf.randomNote();
 		return this;
 	}
 }
