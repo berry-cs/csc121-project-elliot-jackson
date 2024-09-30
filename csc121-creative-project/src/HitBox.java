26a137a14ff4ed800cd48bfcc8e70ab5d69b5ceb
jPelot <jackson.pelot@vikings.berry.edu>
2024-09-30 10:23:29 -0400

    Add HitBox class

---
+++ csc121-creative-project/src/HitBox.java
@@ -0,0 +1,75 @@
+
+/* Represents a rectangular hit box
+ * The "position" of the box represents the coordinates of
+ * the top left corner
+ */
+
+import processing.core.PApplet;
+
+public class HitBox {
+	private float locX, locY, sizeX, sizeY;
+	
+	public HitBox(float locX, float locY ,float sizeX, float sizeY) {
+		this.locX = locX;
+		this.locY = locY;
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+	}
+	
+	public HitBox(float locX, float locY ,float size) {
+		this.locX = locX;
+		this.locY = locY;
+		this.sizeX = size;
+		this.sizeY = size;
+	}
+	
+	/** Creates a hitbox at the specified center */
+	public static HitBox newByCenter(float centerX, float centerY, float sizeX, float sizeY) {
+		float locX = centerX - (sizeX/2);
+		float locY = centerY - (sizeY/2);
+		return new HitBox(locX, locY, sizeX, sizeY);
+	}
+	
+	/** Getters */
+	public float x() {
+		return locX;
+	}
+	
+	public float y() {
+		return locY;
+	}
+	
+	public float width() {
+		return sizeX;
+	}
+	
+	public float height() {
+		return sizeY;
+	}
+	
+	public float centerX() {
+		return locX + (sizeX/2);
+	}
+	
+	public float centerY() {
+		return locY + (sizeY/2);
+	}
+	
+	/** Mutation */
+	public void addY(float y) {
+		this.locY = this.locY + y;
+	}
+	
+	/** Returns true if point is inside hitbox */
+	public boolean isInside(float x, float y) {
+		x = x - locX;
+		y = y - locY;
+		return (x > 0) && (y > 0) && (x < sizeX) && (y < sizeY);
+	}
+	
+	public void draw(PApplet p) {
+		p.noFill();
+		p.rect(locX, locY, sizeX, sizeY);
+	}
+	
+}
