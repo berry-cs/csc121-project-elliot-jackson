61fc3077c4cd4df5b88baa58fc11053477b29e26
jPelot <jackson.pelot@vikings.berry.edu>
2024-09-30 10:23:56 -0400

    Modify Note class to use HitBox

--- csc121-creative-project/src/Note.java
+++ csc121-creative-project/src/Note.java
@@ -1,23 +1,21 @@
 import processing.core.PApplet;
 
 public class Note {
-	int x;
-	int y;
-	int speed;
+	HitBox loc;
+	float speed;
 	
-	public Note(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Note(float centerX, float centerY) {
+		loc = HitBox.newByCenter(centerX, centerY, 20, 20);
 		speed = 1;
 	}
 	
 	public void update() {
-		this.y -= speed;
+		loc.addY(-speed);
 	}
 	
 	public void draw(PApplet p) {
 		p.fill(255,255,0);
-		p.rect(x, y, 20, 20);
+		p.rect(loc.x(), loc.y(), 20, 20);
 	}
 	
 }
